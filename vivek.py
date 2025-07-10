fe6526d8271e7417861955817efe43fc2230cc50
viveksharma78 <viveksharma95104@gmail.com>
2025-07-10 19:16:03 +0530

    Add files via upload

---
+++ vivek.py
@@ -0,0 +1,9 @@
+name = input("enter your name")
+age = input("enter your age")
+roll_number = input("enter your roll number")
+address = input ("enter your address")
+print("name",name)
+print("age",age)
+print("roll_number",roll_number)
+print("address",address)
+
