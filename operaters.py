9547aa8852f5294ffbec4e97f630911b3e575258
viveksharma78 <viveksharma95104@gmail.com>
2025-07-10 19:18:37 +0530

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,8 @@
+a = int(input(" value of a"))
+b = int(input("value of b"))
+sum = a+b
+sub = a-b
+divide = a/b
+module = a%b
+multiply = a*b
+print(f" sum is {sum} \n divide is {divide} \n substraction is {sub}\n multiplication is {multiply}\n modulus is {module}")
