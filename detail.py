d3fe067941db41f2674d7e388097f040123d610b
viveksharma78 <viveksharma95104@gmail.com>
2025-07-10 19:17:59 +0530

    Add files via upload

---
+++ detail.py
@@ -0,0 +1,4 @@
+user_name = input("enter your name")
+user_age = int(input("enter your age"))
+user_roll_number = int(input ("enter your roll_number"))
+print(f"hii your name is{user_name} and your age is {user_age} and your roll_number is {user_roll_number}")
