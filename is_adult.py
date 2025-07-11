426b9097d3a962166fec91add50fb1046f71fd91
Vivek Sharma <viveksharma95104@gmail.com>
2025-07-11 16:35:32 +0530

    Add files via upload

---
+++ is_adult.py
@@ -0,0 +1,11 @@
+#{user_name= (input("my name is "))
+#user_age = int(input("my age is "))
+
+#is_adult = user_age >=18
+
+#print(f"your name is{user_name} and your age is{user_age} are you adult { is_adult }")
+#}
+name = (input("my name is"))
+age = int(input("my age is"))
+is_adult = age > 18
+print(f"my name is {name} \n my age is {age} \n are you adult\n  {is_adult}")
